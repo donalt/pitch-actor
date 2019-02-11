a3432baa8e50c763434b99a31d33792181e941bc
larpe <larpe@kth.se>
2019-02-11 21:32:54 +0100

    Separate GUI and Audio, disable Buttons when recording

--- src/audio.py
+++ src/audio.py
@@ -1,10 +1,24 @@
+import numpy as np
 import librosa as rosa
 import pyaudio
-import numpy as np
+import wave
+import time
 from struct import unpack
 
 class Audio():
-	def __init__(self, path=None, binary=None, sr=11025):
+	RECORD_RATE = 11025
+	TEMP_WAV = 'tmp.wav'
+	CURSOR_OFFSET = 0.1 # How many seconds to delay the playback cursor.
+
+	def __init__(self, gui):
+		self.gui = gui
+		self.pyaudio = pyaudio.PyAudio()
+		self.wf = None     # The loaded binary .wav file.
+		self.stream = None # Stream used when playing and recording wav.
+		self.start_ratio = 0 # [0,1] from where to start playback.
+		self._recording = False
+
+	def _load_audio(self, path=None, binary=None, sr=11025):
 		if path is not None:
 			self.y, _ = rosa.load(path, sr=sr)
 		elif binary is not None:
@@ -15,6 +29,71 @@ class Audio():
 		self.sr = sr
 		self.samples = self.y.size
 		self.duration = self.samples / self.sr # Length in seconds.
+
+	def load_wav(self, file_name):
+		if self.wf is not None:
+			self.wf.close()
+		self.wf = wave.open(file_name, 'rb')
+		# Open non-binary wave file and get pitch and magnitude.
+		self._load_audio(path=file_name, sr=11025)
+		self.pitch, self.mag = self.pitch_mag()
+		self.wf.rewind()
+		self.gui.draw_pitch_mag(self.pitch, self.mag)
+
+	def play(self):
+		# Stop playback if there is one, and rewind to starting position.
+		if self.stream is not None:
+			self.stream.stop_stream()
+			self.stream.close()
+		self.wf.setpos(int(self.wf.getnframes() * self.start_ratio))
+
+		self.stream = self.pyaudio.open(
+			rate=self.wf.getframerate(),
+			channels=1,
+			format=self.pyaudio.get_format_from_width(self.wf.getsampwidth()),
+			output=True,
+			stream_callback=self._play_callback
+		)
+		self.play_time = time.clock()
+
+	def _play_callback(self, in_data, frame_count, time_info, status):
+		data = self.wf.readframes(frame_count)
+		return (data, pyaudio.paContinue)
+
+	def stop_recording(self):
+		self.stream.stop_stream()
+		self.stream.close()
+		self.load_wav(self.TEMP_WAV)
+		self._recording = False
+
+	def start_recording(self):
+		if self.wf is not None:
+			self.wf.close()
+		self.wf = wave.open(self.TEMP_WAV, 'wb')
+		self.wf.setnchannels(1)
+		self.wf.setsampwidth(self.pyaudio.get_sample_size(pyaudio.paInt16))
+		self.wf.setframerate(self.RECORD_RATE)
+
+		self.stream = self.pyaudio.open(
+			rate=self.RECORD_RATE,
+			channels=1,
+			format=pyaudio.paInt16,
+			input=True,
+			stream_callback=self._record_callback
+		)
+		self._recording = True
+
+	def _record_callback(self, in_data, frame_count, time_info, status):
+		self.wf.writeframes(in_data)
+		return (None, pyaudio.paContinue)
+
+	def stop(self):
+		if self.stream and self.stream.is_active():
+			self.stream.stop_stream()
+
+	def rewind(self):
+		self.start_ratio = 0
+		self.stop()
 
 	def pitch_mag(self, n_fft=1024, hop_length=256, fmin=20, fmax=300, threshold=25):
 		self.threshold = threshold
@@ -36,6 +115,24 @@ class Audio():
 				pitch[t] = None
 
 		return pitch, magnitude
+
+	#### Getters ####
+	def loaded(self):
+		return self.wf is not None
+
+	def playing(self):
+		return self.stream.is_active()
+
+	def recording(self):
+		return self._recording
+
+	def play_ratio(self):
+		play_time = time.clock() - self.play_time - self.CURSOR_OFFSET
+		return (play_time / self.duration) + self.start_ratio
+
+	#### Setters ####
+	def set_start(self, start_ratio):
+		self.start_ratio = start_ratio
 
 def binwav2dec(bin_data):
 	npts = int(len(bin_data)/2)
--- src/gui.py
+++ src/gui.py
@@ -3,8 +3,6 @@ import numpy as np
 from tkinter import filedialog
 from tkinter import *
 import audio
-import pyaudio
-import wave
 import time
 
 # TODO: Put as much audio stuff as possible in separate file.
@@ -16,16 +14,8 @@ class PitchGUI:
 	PITCH_MAX_Y = 150
 	PITCH_MIN_Y = 0
 
-	RECORD_RATE = 11025
-	TEMP_WAV = 'tmp.wav'
-
 	def __init__(self):
-		# Audio stuff
-		self.pyaudio = pyaudio.PyAudio()
-		self.wf = None
-		self.stream = None
-		self.start_ratio = 0
-		self.recording = False
+		self.audio = audio.Audio(self)
 
 		self.root = Tk()
 		self.root.wm_title('PitchActor 0.0.1')
@@ -65,11 +55,14 @@ class PitchGUI:
 		controls = Frame(window)
 		controls.pack(fill=X)
 
+		self.stop_btn = Button(controls, text='Stop', command=self.stop_button)
+		self.stop_btn.pack(side=LEFT, ipady=10, ipadx=20)
+
+		self.play_btn = Button(controls, text='Play', command=self.play_button)
+		self.play_btn.pack(side=LEFT, ipady=10, ipadx=20)
+
 		self.rewind_btn = Button(controls, text='Rewind', command=self.rewind_button)
 		self.rewind_btn.pack(side=LEFT, ipady=10, ipadx=20)
-
-		self.play_btn = Button(controls, text='Play', command=self.play_wav)
-		self.play_btn.pack(side=LEFT, ipady=10, ipadx=20)
 
 		self.record_btn = Button(controls, text='Record', command=self.record_button)
 		self.record_btn.pack(side=LEFT, ipady=10, ipadx=20)
@@ -77,7 +70,7 @@ class PitchGUI:
 		self.save_btn = Button(controls, text='Save', command=self.save_wav)
 		self.save_btn.pack(side=LEFT, ipady=10, ipadx=20)
 
-		self.load_wav('../sound/test.wav') # TESTING
+		self.audio.load_wav('../sound/test.wav') # TESTING
 		window.mainloop()
 
 	def draw_curve(self, canvas, x, y):
@@ -96,113 +89,70 @@ class PitchGUI:
 			if np.all(np.isfinite(y[i:i+2])):
 				canvas.create_line(x[i], y[i], x[i+1], y[i+1], width=2)
 
+	def draw_pitch_mag(self, pitch, mag):
+		x = np.linspace(0, self.CANVAS_W, pitch.size)
+		self.draw_curve(self.pitch_graph, x, pitch)
+		self.draw_curve(self.mag_graph, x, mag)
+
 	def move_cursor(self, cursor, x):
 		self.pitch_graph.coords(cursor, [x, 0, x, self.CANVAS_H])
 		self.pitch_graph.update_idletasks()
 
-	def animate_cursor(self):
-		if self.stream.is_active():
-			x = self.CANVAS_W * ((time.clock() - self.play_t) / self.wav.duration + self.start_ratio)
+	def play_callback(self):
+		if self.audio.playing():
+			x = self.CANVAS_W * self.audio.play_ratio()
 			self.move_cursor(self.cursor_line, x)
-			self.root.after(20, self.animate_cursor)
+			self.root.after(20, self.play_callback)
 		else:
+			self.record_btn.config(state=NORMAL)
 			self.move_cursor(self.cursor_line, 0)
 
 	def set_threshold(self, value):
 		value = self.CANVAS_H - value
 		self.mag_graph.coords(self.threshold_line, [0, value, self.CANVAS_W, value])
 
-	def load_wav(self, file_name):
-		if self.wf is not None:
-			self.wf.close()
-		self.wf = wave.open(file_name, 'rb')
-
-		# Open non-binary wave file and get pitch and magnitude.
-		self.wav = audio.Audio(path=file_name, sr=11025)
-		self.pitch, self.mag = self.wav.pitch_mag()
-		self.wf.rewind()
-		# Draw the pitch and magnitude.
-		x = np.linspace(0, self.CANVAS_W, self.pitch.size)
-		self.draw_curve(self.pitch_graph, x, self.pitch)
-		self.draw_curve(self.mag_graph, x, self.mag)
-
 	def save_wav(self):
-		pass
-
-	def play_wav(self):
-		# Stop playback if there is one, and rewind to starting position.
-		if self.stream is not None:
-			self.stream.stop_stream()
-			self.stream.close()
-		self.wf.setpos(int(self.wf.getnframes() * self.start_ratio))
-
-		self.stream = self.pyaudio.open(
-			rate=self.wf.getframerate(),
-			channels=1,
-			format=self.pyaudio.get_format_from_width(self.wf.getsampwidth()),
-			output=True,
-			stream_callback=self.play_callback
-		)
-		self.root.after(0, self.animate_cursor)
-		self.play_t = time.clock()
-
-	def play_callback(self, in_data, frame_count, time_info, status):
-		data = self.wf.readframes(frame_count)
-		return (data, pyaudio.paContinue)
-
-	def record_wav(self):
-		if self.wf is not None:
-			self.wf.close()
-		self.wf = wave.open(self.TEMP_WAV, 'wb')
-		self.wf.setnchannels(1)
-		self.wf.setsampwidth(self.pyaudio.get_sample_size(pyaudio.paInt16))
-		self.wf.setframerate(self.RECORD_RATE)
-
-		self.stream = self.pyaudio.open(
-			rate=self.RECORD_RATE,
-			channels=1,
-			format=pyaudio.paInt16,
-			input=True,
-			stream_callback=self.record_callback
-		)
-
-	def record_callback(self, in_data, frame_count, time_info, status):
-		self.wf.writeframes(in_data)
-		return (None, pyaudio.paContinue)
-
-	def placeholder(self):
 		pass
 
 	########### Menu Options ############
 	def open_wav_file(self):
 		path = filedialog.askopenfilename(title='Select wav file', filetypes=(("wav file","*.wav"),))
 		if len(path) > 0:
-			self.load_wav(path)
+			self.audio.load_wav(path)
 
 	###########    Buttons   ############
-	def record_button(self):
-		if self.recording:
-			self.stream.stop_stream()
-			self.stream.close()
-			self.load_wav(self.TEMP_WAV)
-			self.recording = False
-		else:
-			self.record_wav()
-			self.recording = True
-
 	def rewind_button(self):
-		if self.stream and self.stream.is_active():
-				self.stream.stop_stream()
+		self.audio.rewind()
 		self.move_cursor(self.cursor_line, 0)
 		self.move_cursor(self.cursor_start_line, 0)
-		self.start_ratio = 0
+
+	def play_button(self):
+		self.record_btn.config(state=DISABLED)
+		self.audio.play()
+		self.root.after(0, self.play_callback)
+
+	def stop_button(self):
+		if self.audio.recording():
+			self.record_button() # Stop btn functions like Record btn when recording.
+		else:
+			self.audio.stop()
+			self.move_cursor(self.cursor_line, 0)
+
+	# TODO: Disable other buttons while recording
+	def record_button(self):
+		if self.audio.recording():
+			self.audio.stop_recording()
+			self.play_btn.config(state=NORMAL)
+			self.rewind_btn.config(state=NORMAL)
+		else:
+			self.audio.start_recording()
+			self.play_btn.config(state=DISABLED)
+			self.rewind_btn.config(state=DISABLED)
 
 	########### Mouse Events ############
 	# Move cursor and stop audio if playing.
 	def click_playback_cursor(self, event):
-		if self.wf is not None:
-			if self.stream and self.stream.is_active():
-				self.stream.stop_stream()
-
+		if self.audio.loaded():
+			self.audio.stop()
+			self.audio.set_start(event.x / self.CANVAS_W)
 			self.move_cursor(self.cursor_start_line, event.x)
-			self.start_ratio = event.x / self.CANVAS_W
